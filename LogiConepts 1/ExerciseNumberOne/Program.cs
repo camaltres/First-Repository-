eb54eefaf9b9673b4d494487e45035b17aed12a1
Brayan Herrera Hernández <brayanherrerahernandez6@gmail.com>
2026-02-13 14:01:15 -0500

    Solution to exercise number 1 - Even or odd number

---
+++ LogiConepts 1/ExerciseNumberOne/Program.cs
@@ -0,0 +1,26 @@
+﻿using Reusable_code;
+
+var answer = string.Empty;
+var options = new List<string> { "s", "n" }; // Fixed variable name
+
+do
+{
+    var number = ConsoleExtension.GetInt("Ingrese un numero entero");
+
+    if (number % 2 == 0)
+    {
+        Console.WriteLine($"El numero {number}, es par");
+    }
+    else
+    {
+        Console.WriteLine($"El numero {number}, es impar");
+    }
+    do
+    {
+        answer = ConsoleExtension.GetValidOptions("Desea continuar [S]i, [N]O?;", options); 
+    }
+    while (!options.Any(x => x.Equals(answer, StringComparison.CurrentCultureIgnoreCase))); 
+
+} while (answer!.Equals("s", StringComparison.CurrentCultureIgnoreCase)); 
+
+Console.WriteLine("El programa termino");
