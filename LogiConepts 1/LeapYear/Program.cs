ea887c93022f6e0d85df5f08b08b08dd5002b4e2
Brayan Herrera Hernández <brayanherrerahernandez6@gmail.com>
2026-02-14 16:53:59 -0500

    Solution to exercise number 5 - Leap year

---
+++ LogiConepts 1/LeapYear/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.WriteLine("______________________________________________");
+Console.WriteLine("Programa para saber si un año es bisiesto o no");
+Console.WriteLine("______________________________________________");
+
+Console.WriteLine("Ingrese el año");
+var year = Console.ReadLine();
+if (!double.TryParse(year, out double yeardouble))
+{
+    Console.WriteLine("Ingre un dato valido");
+    return;
+}
+
+if ((yeardouble % 4 == 0) && (yeardouble % 100 != 0))
+{
+    Console.WriteLine($"El año: {yeardouble} si es bisisesto");
+}
+else if ((yeardouble % 100 == 0) && (yeardouble % 400 != 0))
+{
+    Console.WriteLine($"El año: {yeardouble} no es bisiesto");
+}
+else if ((yeardouble % 100 == 0) && (yeardouble % 400 == 0))
+{
+    Console.WriteLine($"El año: {yeardouble} si es bisiesto");
+}
+else
+{
+    Console.WriteLine($"El año: {yeardouble} no es bisiesto");
+}
