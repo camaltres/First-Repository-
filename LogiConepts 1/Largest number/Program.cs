661abfef9f1453ce434ec5f5eccd4a926da9cfd1
Brayan Herrera Hernández <brayanherrerahernandez6@gmail.com>
2026-02-13 23:21:44 -0500

    Solution to exercise number 2 - Largest number

---
+++ LogiConepts 1/Largest number/Program.cs
@@ -0,0 +1,52 @@
+﻿Console.WriteLine("__________________________________________________________");
+Console.WriteLine("Programa para encontrar el número mayor entre 3 números");
+Console.WriteLine("__________________________________________________________");
+
+
+Console.WriteLine("Ingrese el primer número");
+var number1 = Console.ReadLine();
+if (!int.TryParse(number1, out int result1))
+{
+    return;
+}
+
+
+Console.WriteLine("Ingrese el Segundo número");
+var number2 = Console.ReadLine();
+if (!int.TryParse(number2, out int result2))
+{
+    return;
+}
+
+Console.WriteLine("Ingrese el Tercer número");
+var number3 = Console.ReadLine();
+if (!int.TryParse(number3, out int result3))
+{
+    return;
+}
+
+
+if ((result1 >= 0) && (result2 >= 0) && (result3 >= 0))
+{
+    if ((result1 > result2) && (result1 > result3))
+    {
+        Console.WriteLine($"El número mayor es: {result1}");
+    }
+    else if ((result2 > result1) && (result2 > result3))
+    {
+        Console.WriteLine($"El número mayor es: {result2}");
+    }
+    else if ((result3 > result1) && (result3 > result2))
+    {
+        Console.WriteLine($"El número mayor es: {result3}");
+    }
+    else
+    {
+        Console.WriteLine("Los 3 números son iguales");
+    }
+}
+else 
+{
+    Console.WriteLine("Escriba un numero entero positivo mayor o igual a 0");
+}
+
