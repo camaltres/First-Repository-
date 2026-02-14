a59fa0214a3a7154bca89a73174036cdb8e97a5b
Brayan Herrera Hernández <brayanherrerahernandez6@gmail.com>
2026-02-14 14:47:50 -0500

    Solution to exercise number 3 - Ordering of three numbers

---
+++ LogiConepts 1/OrderingOfThreeNumbers/Program.cs
@@ -0,0 +1,104 @@
+﻿Console.WriteLine("_______________________________");
+Console.WriteLine("Programa para ordenar 3 números");
+Console.WriteLine("_______________________________");
+
+Console.WriteLine("Ingrese el primer número");
+var number1 = Console.ReadLine();
+if (!int.TryParse(number1, out int result1)) 
+{ 
+    Console.WriteLine("El valor ingresado no es un número válido."); 
+    return;
+}
+
+Console.WriteLine("Ingrese el segundo número");
+var number2 = Console.ReadLine();
+if (!int.TryParse(number2, out int result2))
+{
+    Console.WriteLine("El valor ingresado no es un número válido.");
+    return;
+}
+
+Console.WriteLine("Ingrese el tercero número");
+var number3 = Console.ReadLine();
+if (!int.TryParse(number3, out int result3))
+{
+    Console.WriteLine("El valor ingresado no es un número válido.");
+    return;
+}
+
+if ((result1 > result2) && (result1 > result3))
+{
+    Console.WriteLine($"El número mayor es: {result1}");
+}
+else if ((result2 > result1) && (result2 > result3))
+{
+    Console.WriteLine($"El número mayor es: {result2}");
+}
+else if ((result3 > result1) && (result3 > result2))
+{
+    Console.WriteLine($"El número mayor es: {result3}");
+}
+else if ((result1 == result2) && (result2 > result3))
+{
+    Console.WriteLine($"El número mayor es: {result1}");
+}
+else if ((result3 == result2) && (result2 > result1))
+{ 
+    Console.WriteLine($"El número mayor es: {result2}");
+}
+else if((result3 == result1) && (result1 > result2))
+{
+    Console.WriteLine($"El número mayor es: {result1}");
+}
+else
+{
+    Console.WriteLine($"El número mayor es: {result1}");
+}
+
+
+if ((result1 > result2) && (result1 < result3) || (result1 > result3) && (result1 < result2))
+{
+    Console.WriteLine($"El numero del medio es: {result1}");
+}
+else if ((result2 > result3) && (result2 < result1) || (result2 > result1) && (result2 < result3))
+{
+    Console.WriteLine($"El numero del medio es: {result2}");
+}
+else if ((result3 > result2) && (result3 < result1) || (result3 > result1) && (result3 < result2))
+{
+    Console.WriteLine($"El numero del medio es: {result3}");
+}
+else
+{
+    Console.WriteLine($"El número del medio es: {result2}");
+}
+
+if ((result1 < result2) && (result1 < result3))
+{
+    Console.WriteLine($"El número menor es: {result1}");
+}
+else if ((result2 < result1) && (result2 < result3))
+{
+    Console.WriteLine($"El número menor es: {result2}");
+}
+else if ((result3 < result1) && (result3 < result2))
+{
+    Console.WriteLine($"El número menor es: {result3}");
+}
+else if ((result1 == result2) && (result2 < result3))
+{
+    Console.WriteLine($"El número menor es: {result1}");
+}
+else if ((result3 == result2) && (result2 < result1))
+{
+    Console.WriteLine($"El número menor es: {result2}");
+}
+else if ((result3 == result1) && (result1 < result2))
+{
+    Console.WriteLine($"El número menor es: {result1}");
+}
+else
+{
+    Console.WriteLine($"El número menor es: {result1}");
+}
+
