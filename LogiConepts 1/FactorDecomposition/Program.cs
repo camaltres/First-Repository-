71886a38c5412f5da815082012776ba73a36541c
Brayan Herrera Hernández <brayanherrerahernandez6@gmail.com>
2026-03-08 17:53:15 -0500

    Solution

---
+++ LogiConepts 1/FactorDecomposition/Program.cs
@@ -0,0 +1,36 @@
+﻿using Reusable_code;
+using System.Net.Http.Headers;
+
+Console.WriteLine("_____________________________________________");
+Console.WriteLine("Programa que descompone un número en factores");
+Console.WriteLine("_____________________________________________");
+
+var number = ConsoleExtension.GetInt("Ingrese el numero a factorizar.....: ");
+
+Console.Write($"{number} = ");
+for (int i = 2; i <= number; i++)
+{
+    int dividor = 0;
+    for (int a = 1; a <= i; a++)
+    {
+        if (i % a == 0)
+        { 
+          dividor++;
+        }
+    
+    }
+    if (dividor == 2)
+    {
+        while (number % i == 0)
+        {
+            Console.Write($" {i} ");
+            number = number / i;
+            if (number / i > 0)
+            {
+                Console.Write(" x ");
+            }
+        }
+    }
+
+
+}
---
+++ LogiConepts 1/OperationsOnAMatrix/Program.cs
@@ -0,0 +1,54 @@
+﻿using Reusable_code;
+
+Console.WriteLine("________________________");
+Console.WriteLine("Programa para una Matriz");
+Console.WriteLine("________________________");
+
+var order = ConsoleExtension.GetInt("Ingrese el orden de la matriz: ");
+int[,] matrix = new int[order,order];
+
+//This loop fills the matrix with the following formula (i + 1) - j
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        matrix[i, j] = (i + 1) - j;
+    }
+}
+//I created these variables to find the maximum, minimum, and sum of the matrix
+int x = 0;
+int y = 0;
+int z = 0;
+
+//This loop prints the position of a number found in the array, in addition to performing other operations.
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+
+        Console.Write($"{matrix[i,j].ToString()}\t");
+
+        //Each time j is equal to the number being subtracted, a line break is created.
+        if (j == (order - 1))
+        {
+            Console.WriteLine("\n");
+        }
+        //Take the largest number
+        if (x < matrix[i, j])
+        { 
+            x = matrix[i, j];
+        }
+        //Take the smallest number
+        if (y > matrix[i, j])
+        {
+            y = matrix[i, j];
+        }
+        //addition
+        z = z + matrix[i, j];
+    }
+
+}
+//Print the values
+Console.WriteLine($"La suma es: {z}");
+Console.WriteLine($"El número mayor es: {x}");
+Console.WriteLine($"El numero menor es: {y}");
